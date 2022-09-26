edd3baae82c3a9b05ce7eaabc998e3924cf9d91f
KayckSchuenck <ycferreiras@gmail.com>
2022-09-26 15:13:48 -0300

    Testes E2E prontos

--- cypress/e2e/menuTests.cy.js
+++ cypress/e2e/menuTests.cy.js
@@ -10,6 +10,7 @@ describe("Get random song", () => {
     cy.get("#submit").click();
     cy.wait("@getPosts");
     cy.contains("Random").click();
+    cy.url().should("be.equal", "http://localhost:3000/random");
     cy.get("article").should("have.length", 1);
   });
 });
@@ -17,17 +18,23 @@ describe("Get random song", () => {
 describe("Get top songs", () => {
   it("clicking the top button goes to /top route and shows most upvoted recommendations", () => {
     const name = faker.music.songName();
-    const name2 = faker.music.songName();
     cy.visit("localhost:3000");
     cy.get("#name").type(name);
     cy.get("#link").type("https://www.youtube.com/watch?v=ZKecJ1Dk-WA");
     cy.get("#submit").click();
-    cy.get("#name").type(name2);
-    cy.get("#link").type("https://www.youtube.com/watch?v=ZKecJ1Dk-WA");
-    cy.intercept("GET", "/recommendations").as("getPosts");
-    cy.get("#submit").click();
-    cy.wait("@getPosts");
     cy.contains("Top").click();
-    cy.get("article").should("have.length.greaterThan", 1);
+    cy.url().should("be.equal", "http://localhost:3000/top");
+    cy.get("article").should("have.length.greaterThan", 0);
+  });
+});
+
+describe("Get all songs", () => {
+  it("clicking the home button goes to / route and shows all recommendations", () => {
+    cy.visit("localhost:3000/random");
+    cy.contains("Home").click();
+    cy.url().should("be.equal", "http://localhost:3000/");
+    cy.get("#name").should("exist");
+    cy.get("#link").should("exist");
+    cy.get("#submit").should("exist");
   });
 });
